3dd3f4fc32a2398d7445fe154b8c3557431ebd4e
XBaiLong <2995773594@qq.com>
2021-05-15 19:35:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,4 +29,4 @@ svn co https://github.com/281677160/openwrt-package/trunk/luci-app-filebrowser p
 svn co https://github.com/281677160/openwrt-package/trunk/luci-app-poweroff package/luci-app-poweroff
 
 #git clone https://github.com/281677160/openwrt-package package/281677160
-#rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
